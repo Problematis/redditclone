d87fc82d5ba2aac9b6110346b6b405a737f2f456
Problematis <33418961+Problematis@users.noreply.github.com>
2018-03-29 16:23:05 +0100

    b

--- redditclone/urls.py
+++ redditclone/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 import accounts.views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-#    url(r'^signup/', accounts.views.signup, name="signup"),
+    url(r'^admin/',admin.site.urls),
+    url(r'^signup/',accounts.views.signup, name="signup"),
 ]
