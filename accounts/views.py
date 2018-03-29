4ea0b5719d67a79c8b557f766fbcb0ffbea4b71d
Problematis <33418961+Problematis@users.noreply.github.com>
2018-03-29 17:44:04 +0100

    b

--- accounts/views.py
+++ accounts/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def signup(request):
+    if request.method == 'POST':
+        print("the post worked")
+    else:
     return render(request, 'accounts/signup.html')
