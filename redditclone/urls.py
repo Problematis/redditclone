f0c7b9195ee060160141734e66a6a3b979cb7acc
Problematis <33418961+Problematis@users.noreply.github.com>
2018-03-29 15:21:47 +0100

    urls.py

--- redditclone/urls.py
+++ redditclone/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls inport url
 import accounts.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^signup/', accounts.views.signup),
 ]
