0219b0f5d50a5c7e086622c5064ffee2592f2187
Problematis <33418961+Problematis@users.noreply.github.com>
2018-03-29 18:10:53 +0100

    b

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
-        if request.POST['password1'] == request.POST['password2']
+        if request.POST['password1'] == request.POST['password2']:
             User.objects.create_user(request.POST['username'], password=request.POST['password1'])
             return render(request, 'accounts/signup.html')
         else:
